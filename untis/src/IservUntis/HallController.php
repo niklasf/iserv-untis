571cf864a40be8bf4afc4d66067f3b0d3db19b82
Niklas Fiekas <niklas.fiekas@googlemail.com>
2013-08-19 16:06:14 +0200

    Forgot to add controllers.

---
+++ untis/src/IservUntis/HallController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace IservUntis;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class HallController
+{
+    private $records = array();
+
+    private $lastChange;
+
+    public function __construct()
+    {
+        $this->lastChange = filemtime('/var/untis/GPU009.TXT');
+
+        $handle = fopen('/var/untis/GPU009.TXT', 'r');
+        if ($handle) {
+            while ($data = fgetcsv($handle)) {
+                $this->records[] = array(
+                    'hall' => utf8_encode($data[0]),
+                    'teacher' => utf8_encode($data[1]),
+                    'day' => intval($data[2]),
+                    'slot' => intval($data[3]),
+                    'points' => intval($data[4]),
+                );
+            }
+            fclose($handle);
+        }
+    }
+
+    public function getLastChange()
+    {
+        return $this->lastChange;
+    }
+
+    public function getSchedule($hall)
+    {
+        $schedule = array();
+
+        foreach ($this->records as $record) {
+            if ($record['hall'] == $hall) {
+                $schedule[$record['day']][$record['slot']] = $record;
+            }
+        }
+
+        if (!$schedule) {
+            throw new NotFoundHttpException();
+        }
+
+        return $schedule;
+    }
+
+    public function renderSchedule(Request $request, Application $app, $hall)
+    {
+        return $app->render('hall.html.twig', array(
+            'hall' => $hall,
+            'schedule' => $this->getSchedule($hall),
+        ));
+    }
+
+    public function getHallIndex()
+    {
+        $halls = array();
+
+        foreach ($this->records as $record) {
+            $halls[$record['hall']] = $record['hall'];
+        }
+
+        ksort($halls);
+
+        return $halls;
+    }
+}
---
+++ untis/src/IservUntis/IndexController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace IservUntis;
+
+use Symfony\Component\HttpFoundation\Request;
+use IservUntis\ScheduleController;
+
+class IndexController
+{
+    private $scheduleController;
+
+    private $hallController;
+
+    public function __construct()
+    {
+        $this->scheduleController = new ScheduleController();
+        $this->hallController = new HallController();
+    }
+
+    public function renderIndex(Request $request, Application $app)
+    {
+        return $app->render('index.html.twig', array(
+            'classes' => $this->scheduleController->getClassIndex(),
+            'halls' => $this->hallController->getHallIndex(),
+        ));
+    }
+}
